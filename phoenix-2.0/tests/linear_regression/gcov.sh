9f9e14f467289730abedb3e9dc9c15e54fa455fb
zl679 <zhijing>
2019-11-14 16:00:05 -0500

    fix gcov

--- phoenix-2.0/tests/linear_regression/gcov.sh
+++ phoenix-2.0/tests/linear_regression/gcov.sh
@@ -2,4 +2,5 @@
 fname=${PWD##*/}-seq
 echo ${fname}
 gcc -fprofile-arcs -ftest-coverage ${fname}.c
+./a.out linear_regression_datafiles/key_file_50MB.txt 
 gcov -b -c -f ${fname}.c | tee gcov.profiling
